pallet/pallet-jclouds

commit c8ad1e3879c898b13a41e1ea94b33c360dcab31a

Use only :image-id for template if specified
Stop other information in the template from preventing a match.

src/pallet/compute/jclouds.clj
@@ -447,8 +447,14 @@
     (logging/debug (str "  authorizing " public-key-path)))
   (when init-script
     (logging/debug (str "  init script\n" init-script)))
-  (let [options (->> [:image :hardware :location :network :qos]
-                     (select-keys group)
+  (let [;; when we have an id, just use that so other keys don't prevent a
+        ;; match
+        group-spec (update-in group [:image]
+                              #(if (:image-id %)
+                                 (select-keys % [:image-id])
+                                 %))
+        options (->> [:image :hardware :location :network :qos]
+                     (select-keys group-spec)
                      vals
                      (reduce merge))
         options (if (:default-os-family group)

commit b21b654955f91c6fb2abf21fdd15f44df399c9bd

Don't remove template options when using :image-id
Fixes issue introduced in c8ad1e3879c898b13a41e1ea94b33c360dcab31a

src/pallet/compute/jclouds.clj
@@ -452,7 +452,11 @@
         ;; match
         group-spec (update-in group [:image]
                               #(if (:image-id %)
-                                 (select-keys % [:image-id])
+                                 (apply
+                                  dissoc %
+                                  (remove
+                                   (fn [kw] (= :image-id kw))
+                                   (keys @#'org.jclouds.compute2/template-map)))
                                  %))
         options (->> [:image :hardware :location :network :qos]
                      (select-keys group-spec)