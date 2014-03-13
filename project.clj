4faa93166e9215d477e3be68745c6a25f3027130
Hugo Duncan <hugo@hugoduncan.org>
2014-03-13 10:04:16 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (def pallet-ver "0.8.0-RC.8")
 (def jclouds-ver "1.5.5")
 
-(defproject com.palletops/pallet-jclouds "1.5.4"
+(defproject com.palletops/pallet-jclouds "1.7.0-SNAPSHOT"
   :description "A pallet provider for using jclouds."
   :url "https://github.com/pallet/pallet-vmfest"
   :license {:name "Eclipse Public License"
